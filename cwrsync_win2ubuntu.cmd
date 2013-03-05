9db8d64f4a945003408e653488acbeb0813cf348
linfeng <stonelin119@gmail.com>
2013-03-05 12:20:33 +0800

    add Django CSRF framework

---
+++ cwrsync_win2ubuntu.cmd
@@ -0,0 +1,48 @@
+@ECHO OFF
+REM *****************************************************************
+REM
+REM CWRSYNC.CMD - Batch file template to start your rsync command (s).
+REM
+REM By Tevfik K. (http://itefix.no)
+REM *****************************************************************
+
+REM Make environment variable changes local to this batch file
+SETLOCAL
+
+REM ** CUSTOMIZE ** Specify where to find rsync and related files (C:\CWRSYNC)
+SET CWRSYNCHOME=%PROGRAMFILES(x86)%\CWRSYNC
+
+REM Set HOME variable to your windows home directory. That makes sure 
+REM that ssh command creates known_hosts in a directory you have access.
+SET HOME=C:\Users\stone
+
+REM Make cwRsync home as a part of system PATH to find required DLLs
+SET CWOLDPATH=%PATH%
+SET PATH=%CWRSYNCHOME%\BIN;%PATH%
+
+REM Windows paths may contain a colon (:) as a part of drive designation and 
+REM backslashes (example c:\, g:\). However, in rsync syntax, a colon in a 
+REM path means searching for a remote host. Solution: use absolute path 'a la unix', 
+REM replace backslashes (\) with slashes (/) and put -/cygdrive/- in front of the 
+REM drive letter:
+REM 
+REM Example : C:\WORK\* --> /cygdrive/c/work/*
+REM 
+REM Example 1 - rsync recursively to a unix server with an openssh server :
+REM
+REM       rsync -r /cygdrive/c/work/ remotehost:/home/user/work/
+REM
+REM Example 2 - Local rsync recursively 
+REM
+REM       rsync -r /cygdrive/c/work/ /cygdrive/d/work/doc/
+REM
+REM Example 3 - rsync to an rsync server recursively :
+REM    (Double colons?? YES!!)
+REM
+REM       rsync -r /cygdrive/c/doc/ remotehost::module/doc
+REM
+REM Rsync is a very powerful tool. Please look at documentation for other options. 
+REM
+
+REM ** CUSTOMIZE ** Enter your rsync command(s) here
+rsync -avz --delete --exclude-from="rsync_exclude_list.txt" --chmod=Du=rwx,go=rx,Fu=rw,og=r /cygdrive/D/Workspace/Code/mysite/site_templates/* linfeng@192.168.1.177:/home/linfeng/workspace/code/mysite/
